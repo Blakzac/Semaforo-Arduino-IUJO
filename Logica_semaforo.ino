03f579e5ec226a5115f448b36e77d06c5e6a94f7
Michell1203 <alvaradobaezmichell@gmail.com>
2026-03-05 23:36:11 -0400

    Agredada la logica del primer semaforo

---
+++ Logica_semaforo.ino
@@ -0,0 +1,23 @@
+void loop() {
+  // ==========================================
+  // FASE 1: SEMÁFORO 1 EN VERDE
+  // ==========================================
+  digitalWrite(11, HIGH); 
+  digitalWrite(13, LOW);  
+  digitalWrite(10, LOW);  
+  
+  digitalWrite(9, HIGH);  
+  digitalWrite(6, HIGH);  
+  
+  digitalWrite(5, HIGH);  
+  digitalWrite(2, HIGH);  
+  delay(5000);
+
+  // S1 cambia a Amarillo
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay(2000);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH); 
+  digitalWrite(10, HIGH); 
+}
